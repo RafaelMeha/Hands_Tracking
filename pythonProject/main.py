109c7699d1653a7cf2f9cd653cb3a261fcee557e
Rafael Meha <rafaelmeha5@gmail.com>
2024-01-08 05:45:23 +0000

    Update main.py added new hand emoji

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,11 +3,22 @@ import mediapipe as mp
 
 gesture_emoji_map = {
     'index_up': '☝️',
+    'full_open_hand': '🖐️',
 }
+
+def is_finger_up(finger_tip, finger_dip, hand_landmarks):
+    return hand_landmarks.landmark[finger_tip].y < \
+           hand_landmarks.landmark[finger_dip].y
+
+def is_full_open_hand(hand_landmarks):
+    return all(is_finger_up(mp_hands.HandLandmark[tip], mp_hands.HandLandmark[dip], hand_landmarks)
+               for tip, dip in [('THUMB_TIP', 'THUMB_IP'), ('INDEX_FINGER_TIP', 'INDEX_FINGER_DIP'),
+                               ('MIDDLE_FINGER_TIP', 'MIDDLE_FINGER_DIP'), ('RING_FINGER_TIP', 'RING_FINGER_DIP'),
+                               ('PINKY_TIP', 'PINKY_DIP')])
+
 def is_index_finger_up(hand_landmarks):
     return hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < \
-        hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y
-
+           hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -31,7 +42,9 @@ while True:
                 hand_landmarks,
                 mp_hands.HAND_CONNECTIONS)
 
-            if is_index_finger_up(hand_landmarks):
+            if is_full_open_hand(hand_landmarks):
+                print(gesture_emoji_map['full_open_hand'])
+            elif is_index_finger_up(hand_landmarks):
                 print(gesture_emoji_map['index_up'])
 
     cv2.imshow('Hands Tracking', image)

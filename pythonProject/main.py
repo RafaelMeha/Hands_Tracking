b062b308d7dc5bcb6bee461c90d7f9c6ff8f7238
Rafael Meha <rafaelmeha5@gmail.com>
2024-01-08 04:43:36 +0000

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,28 +1,43 @@
 import cv2
 import mediapipe as mp
 
+gesture_emoji_map = {
+    'index_up': '☝️',
+}
+def is_index_finger_up(hand_landmarks):
+    return hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < \
+        hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y
+
+
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-circle_radius = 5
+hands = mp_hands.Hands()
 
 cap = cv2.VideoCapture(0)
-hands = mp_hands.Hands()
+
 while True:
     success, image = cap.read()
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
     results = hands.process(image)
+
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
                 image,
                 hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=circle_radius),
-                mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2)
-            )
-    cv2.imshow('MediaPipe Hands', image)
+                mp_hands.HAND_CONNECTIONS)
+
+            if is_index_finger_up(hand_landmarks):
+                print(gesture_emoji_map['index_up'])
+
+    cv2.imshow('Hands Tracking', image)
+
     if cv2.waitKey(1) & 0xFF == 27:
         break
+
+cap.release()
+cv2.destroyAllWindows()
